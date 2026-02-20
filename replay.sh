c717076368343b8ecb3462ca0d5cfcb0574dbf36
Terence LEE <93152457+telee0@users.noreply.github.com>
2026-02-20 12:48:29 +0800

    Add files via upload

--- replay.sh
+++ replay.sh
@@ -1,31 +1,35 @@
 #!/usr/bin/env bash
 
-IN_DIR="/a2/iot-fixed"
+IN_DIR="/a2/iot"
+OUT_DIR="/a2/iot-fixed"
+FIND_PATTERN="*.pcap*"  # Matches .pcap, .pcapng, .pcap0, .pcap1, ..
+LOG_FILE="replay.log"
 IFACE="enp6s19"
 MAX_ITER=100     # configurable number of iterations
+PPS="2000"
 
-counter=1
-
-cd "$IN_DIR" || {
-    echo "Failed to enter directory: $IN_DIR"
+cd "$OUT_DIR" || {
+    echo "Failed to enter directory: $OUT_DIR"
     exit 1
 }
 
+counter=1
+
 while (( counter <= MAX_ITER )); do
-    echo "$(date) Replaying packets, iteration = $counter / $MAX_ITER"
-
-    for file in *.pcap; do
-        [[ -e "$file" ]] || {
-            echo "No pcap files found"
-            break
-        }
-
-        echo "Replaying $file"
-        tcpreplay -i "$IFACE" -p 500 "$file" > /dev/null
-    done
-
+	echo "Replaying packets, iteration = $counter / $MAX_ITER"
+	echo
+	find . -maxdepth 1 -type f -name "$FIND_PATTERN" -print0 \
+	| sort -z \
+	| while IFS= read -r -d '' file; do
+		echo "[$(date +'%T')] replaying $file"
+		tcpreplay -i "$IFACE" -p "$PPS" "$file"
+	done
     ((counter++))
 done
 
+echo
 echo "Completed $MAX_ITER iterations"
+echo
+
+exit
 
